e2691f527ad928a0700ec0ee693cb9c0cbfac924
Edwin Peraza <edwinperaza87@gmail.com>
2018-08-05 21:03:32 -0400

    Add backstack

--- app/src/main/java/com/example/edwinperaza/globallogictest/productDetail/ProductDetailActivity.java
+++ app/src/main/java/com/example/edwinperaza/globallogictest/productDetail/ProductDetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.edwinperaza.globallogictest.productDetail;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.example.edwinperaza.globallogictest.MainActivity;
 import com.example.edwinperaza.globallogictest.R;
@@ -13,31 +15,33 @@ import com.example.edwinperaza.globallogictest.utils.bus.BusProvider;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
-        private ProductDetailPresenter presenter;
+    private ProductDetailPresenter presenter;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_product_detail);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-            ProductModelObject product = (ProductModelObject) getIntent().getSerializableExtra(MainActivity.PRODUCT_TAG);
+        ProductModelObject product = (ProductModelObject) getIntent().getSerializableExtra(MainActivity.PRODUCT_TAG);
 
-            presenter = new ProductDetailPresenter(
-                    new ProductDetailModel(product),
-                    new ProductDetailView(this, BusProvider.getInstance()));
+        presenter = new ProductDetailPresenter(
+                new ProductDetailModel(product),
+                new ProductDetailView(this, BusProvider.getInstance()));
 
-        }
+    }
 
-        @Override
-        public void onResume() {
-            super.onResume();
-            BusProvider.register(presenter);
-            presenter.onResumed();
-        }
+    @Override
+    public void onResume() {
+        super.onResume();
+        BusProvider.register(presenter);
+        presenter.onResumed();
+    }
 
-        @Override
-        public void onPause() {
-            super.onPause();
-            BusProvider.getInstance().unregister(presenter);
-        }
+    @Override
+    public void onPause() {
+        super.onPause();
+        BusProvider.getInstance().unregister(presenter);
+    }
+
 }
